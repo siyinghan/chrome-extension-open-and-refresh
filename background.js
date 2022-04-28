39ca32405449d86e62d7a603b57ec4d491fc601b
siyinghan <50146714+siyinghan@users.noreply.github.com>
2022-04-29 00:18:56 +0200

    Search field (#3)
    * change search fields
    
    * update .gitignore

--- background.js
+++ background.js
@@ -1,12 +1,23 @@
 const baseURL = "https://s.weibo.com/weibo?q=";
 
 const urls = [
+  baseURL + encodeURI("龚俊粉丝"),
   baseURL + encodeURI("龚俊超话"),
+  baseURL + encodeURI("龚俊工作室"),
+  baseURL + encodeURI("龚俊站姐"),
+  baseURL + encodeURI("龚俊新剧"),
+  baseURL + encodeURI("龚俊壁纸"),
+  baseURL + encodeURI("龚俊怎么了"),
+  baseURL + encodeURI("龚俊进组"),
+  baseURL + encodeURI("龚俊北京"),
+  baseURL + encodeURI("龚俊代言"),
+  baseURL + encodeURI("龚俊杂志"),
+  baseURL + encodeURI("龚俊ins"),
+  baseURL + encodeURI("龚俊腹肌"),
+  baseURL + encodeURI("龚俊商务"),
+  baseURL + encodeURI("龚俊徐斯"),
   baseURL + encodeURI("龚俊电视剧"),
-  baseURL + encodeURI("龚俊视频"),
-  baseURL + encodeURI("龚俊lv"),
-  baseURL + encodeURI("龚俊的浪漫非你茉属"),
-  baseURL + encodeURI("龚俊温客行"),
+  baseURL + encodeURI("龚俊韩烨"),
 ];
 
 chrome.runtime.onInstalled.addListener(() => {
