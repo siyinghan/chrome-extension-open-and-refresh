680cea858b9b3488bcb9e4c4175c965fb29364e1
siying <siyinghan@foxmail.com>
2022-09-03 09:41:29 +0200

    update

--- background.js
+++ background.js
@@ -2,18 +2,21 @@ const baseURL = "https://s.weibo.com/weibo?q=";
 
 const urls = [
   baseURL + encodeURI("龚俊"),
+  baseURL + encodeURI("龚俊路透"),
   baseURL + encodeURI("龚俊杂志"),
   baseURL + encodeURI("龚俊代言"),
   baseURL + encodeURI("龚俊商务"),
   baseURL + encodeURI("龚俊直播"),
-  aseURL + encodeURI("龚俊粉丝"),
-  aseURL + encodeURI("龚俊资源"),
-  baseURL + encodeURI("龚俊工作室"),
+  baseURL + encodeURI("龚俊粉丝"),
+  baseURL + encodeURI("龚俊资源"),
   baseURL + encodeURI("龚俊壁纸"),
+  baseURL + encodeURI("龚俊工作室"),
+  baseURL + encodeURI("龚俊电视剧"),
   baseURL + encodeURI("龚俊 狐妖小红娘月红篇"),
   baseURL + encodeURI("龚俊徐斯"),
   baseURL + encodeURI("龚俊我要逆风去"),
   baseURL + encodeURI("龚俊韩烨"),
+  baseURL + encodeURI("龚俊安乐传"),
 ];
 
 chrome.runtime.onInstalled.addListener(() => {
